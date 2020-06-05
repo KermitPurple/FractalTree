cafbbc29b76d1ae6ecba7f132894ab1504b2f615
Shane McDonough <shanemcdman@gmail.com>
2020-06-05 16:44:25 -0700

    Allow the keys to be held

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
     pygame.display.init()
     pygame.display.set_caption("Fractal Tree")
+    pygame.key.set_repeat(5)
     size = Point(650, 650)
     screen = pygame.display.set_mode(size)
     tree = FractalTree(screen, 45, 1, 2);
@@ -16,7 +17,6 @@ def main():
             if event.type == pygame.KEYDOWN:
                 tree.kbin(event.key)
         if tree.update:
-            print("test")
             screen.fill(0)
             tree.draw(Point(size.x / 2, size.y), -90, size.x // 2)
             pygame.display.update()
